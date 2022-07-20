51774f8dfdff682d015f2e0f11a2a8ceab21b562
Matito <matitos@outlook.de>
2022-07-20 23:35:32 +0200

    script.js V20-7-22

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+/* MIT License
+
+Copyright (c) 2022 MatitoDA
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
 const itemCardTemplate = document.querySelector("[data-item-template]")
 const itemCardContainer = document.querySelector("[data-item-container]")
 const searchInput = document.querySelector("[data-search]")
@@ -7,43 +29,60 @@ const searchInput = document.querySelector("[data-search]")
 searchInput.addEventListener("input", e => {
     const value = e.target.value.toLowerCase()
     items.forEach(item => {
-        const isVisible = item.name.toLowerCase().includes(value) || item.ename.toLowerCase().includes(value)
+        const isVisible = item.name.toLowerCase().includes(value) || 
+        item.ename.toLowerCase().includes(value) || 
+        item.mod.toLowerCase().includes(value)
         item.element.classList.toggle("hide", !isVisible)
         console.log(isVisible)
     });
 })
 
-function update(){
-    var id = document.getElementById("filter").value;
-    console.log(x);
-    if (id === "1") {
-        console.log("mod1")
-        items.forEach(item => {
-            const isVisible = item.mod.includes()
-            item.element.classList.toggle("hide", !isVisible)
-        });
+
+
+function update() {
+    var select = document.getElementById('filter');
+    var option = select.options[select.selectedIndex];
+    var text = option.text
+    document.getElementById('search').value = text;
+    if (option.value === "0") {
+        document.getElementById('search').value = "";
+        
     } else {
-        if (id === "2"){
-            console.log("mod2")
-            element.classList.toggle("hide")
+        if (option.value === "1") {
+            /*  */
+            search()
         } else {
-            if (id === "3"){
-                console.log("mod3")
-                element.classList.toggle("hide")
+            if (option.value === "2") {
+                /*  */
             } else {
-                if (id === "0") {
-                    console.log("keine mod")
-                    element.classList.toggle("hide")
-                } else {
-                    console.log("error")
+                if (option.value === "3") {
+                    /*  */
+                }  else {
+                    if (option.value === "4") {
+                        /*  */
+                    }  else {
+                        if (option.value === "5") {
+                            /*  */
+                        }  else {
+                            if (option.value === "6") {
+                                /*  */
+                            }  else {
+                                if (option.value === "7") {
+                                    /*  */
+                                }  else {
+                                    if (option.value === "8") {
+                                        /*  */
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
-
-
-
+update();
 
 fetch("items.json")
 .then(res => res.json())
@@ -53,12 +92,31 @@ fetch("items.json")
     const header = card.querySelector("[data-header]")
     const body = card.querySelector("[data-body]")
     const mod = card.querySelector("[data-mod]")
-/*     const pic = card.querySelector("[data-pic]")
-    pic.textContent = item.pic */
+    const pic = card.querySelector("[data-pic]")
+    pic.textContent = item.pic
+    var piclink = item.pic
+    /* document.getElementById("rezept").src=item.pic; */
     header.textContent = item.name
     body.textContent = item.ename
     mod.textContent = item.mod
     itemCardContainer.append(card)
-    return{ name: item.name, ename: item.ename, mod: item.mod, element: card }
+    return{ name: item.name, ename: item.ename, mod: item.mod, pic: item.pic, element: card, piclink }
     })
 })
+
+console.log(piclink)
+
+
+
+function search() {
+    var select = document.getElementById('filter');
+    var option = select.options[select.selectedIndex];
+    var text = option.text
+    items.forEach(item => {
+        const isVisible = item.name.toLowerCase().includes(text) || 
+        item.ename.toLowerCase().includes(text) || 
+        item.mod.toLowerCase().includes(text)
+        item.element.classList.toggle("hide", !isVisible)
+        console.log(isVisible)
+    });
+}
